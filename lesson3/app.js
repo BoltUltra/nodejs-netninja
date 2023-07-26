5005af9695ffda0a5b7eb9cb1837b5fc66cd88c1
BoltUltra <mayowa.u.sunusi@gmail.com>
2023-07-26 16:24:20 +0100

    ...

--- lesson3/app.js
+++ lesson3/app.js
@@ -1,34 +1,45 @@
 const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const Blog = require("./modules/blog");
 
 // express app
 const app = express();
 
+// connect to mongoDB
+const dbURI =
+  "mongodb+srv://Mayowa:Mayowa123@ninjablog.u44pcw2.mongodb.net/ninja-blog?retryWrites=true&w=majority";
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
+
 // register view engine
 app.set("view engine", "ejs");
 
-// listen for request
-app.listen(3000);
+// middleware and static files
+app.use(express.static("public"));
+app.use(morgan("dev"));
+
+// routes
 
 app.get("/", (req, res) => {
-  const blogs = [
-    {
-      title: "Yoshi finds eggs",
-      snippet: "Lorem ipsum dolor sit amet consectetur",
-    },
-    {
-      title: "Mario finds stars",
-      snippet: "Lorem ipsum dolor sit amet consectetur",
-    },
-    {
-      title: "How to defeat bowser",
-      snippet: "Lorem ipsum dolor sit amet consectetur",
-    },
-  ];
-  res.render("index", { title: "Home", blogs });
+  res.redirect("/blogs");
 });
 
 app.get("/about", (req, res) => {
   res.render("about", { title: "About" });
+});
+
+app.get("/blogs", (req, res) => {
+  Blog.find()
+    .sort({ createdAt: -1 })
+    .then((result) => {
+      res.render("index", { title: "All Blogs", blogs: result });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 app.get("/blogs/create", (req, res) => {
